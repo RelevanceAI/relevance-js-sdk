dcc6ceff4a53d26fdeb1b3b76afd62adf097644e
Christopher Turner <chris.turner@relevanceai.com>
2025-10-08 16:55:39 +1100

    adds attachments to the user message and new exports

--- mod.ts
+++ mod.ts
@@ -1,5 +1,6 @@
 export { Agent } from "./agent.ts";
-export { Client, createClient } from "./client.ts";
+export { type Attachment, Client, createClient } from "./client.ts";
 export { Key } from "./key.ts";
 export { type Region, REGION_AU, REGION_EU, REGION_US } from "./region.ts";
 export { Workforce } from "./workforce.ts";
+export type { Task } from "./task/task.ts";
