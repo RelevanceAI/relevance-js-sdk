2e501a13bdfa494f2a53ccd276e09558c58a05de
Christopher Turner <chris.turner@relevanceai.com>
2026-03-11 10:13:06 +1100

    correcting agent thoughts

--- message/agent.ts
+++ message/agent.ts
@@ -5,6 +5,7 @@ export interface AgentMessageContent {
   type: "agent-message";
   text: string;
   generating?: boolean;
+  thinking?: string[];
   thought_about_tool_calls?: boolean;
 }
 
@@ -26,12 +27,25 @@ export class AgentMessage extends GenericMessage<AgentMessageContent> {
   public get agentId(): string {
     return this.agent?.id ?? "";
   }
+  
+  public get thoughts(): string[] {
+    return this.message.content.thinking ?? [];
+  }
 
   public isGenerating(): boolean {
     return this.message.content.generating ?? false;
   }
 
+  /**
+   * @deprecated
+   * Do not use this method as it is not a reliable indicator of whether the
+   * agent is thinking. It only indicates whether the message content includes
+   * a flag that suggests the agent is considering tool calls, but this may
+   * not always be present or accurate. This method will be removed in next 
+   * major release.
+   */
   public isThought(): boolean {
     return this.message.content.thought_about_tool_calls ?? false;
   }
+
 }
